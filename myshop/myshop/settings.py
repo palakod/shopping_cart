fe316bd326d5ef92f0b1f109a8f5179d79246c1c
Bhanu Prakash Palakodeti <bhanu8262@gmail.com>
2020-07-13 22:37:45 +0530

    Building an online shop.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig'
+    'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig'
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -122,3 +125,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-
+from cart.forms import CartAddProductsForm
 
 def product_list(request, category_slug=None):
     category = None
@@ -17,4 +17,5 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html',{'product': product})
+    cart_product_form = CartAddProductsForm()
+    return render(request, 'shop/product/detail.html',{'product': product, 'cart_product_form': cart_product_form})
