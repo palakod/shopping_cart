215daecc2b80361d6c56a68744f27e4a8a695461
Bhanu Prakash Palakodeti <bhanu8262@gmail.com>
2020-07-13 07:38:41 +0530

    first commit

---
+++ myshop/shop/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulate_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProdutAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulate_fields = {'slug': ('name',)}
